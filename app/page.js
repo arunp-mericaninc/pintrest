78a730acf7f64d90eb6b08007c2c753441cf8d5e
arunp-mericaninc <arunp@mericaninc.com>
2023-07-10 12:32:49 +0530

    added env file and next-auth

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
+      
       <h1>Hello Team</h1>
     </div>
   )
---
+++ components/Home/Header.jsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import React from "react";
+import { HiSearch, HiBell, HiChat } from "react-icons/hi";
+
+const Header = () => {
+  return (
+    <div className="flex items-center gap-6 p-2">
+      <Image
+        src="/images/pintrest.png"
+        alt="pintrest"
+        width={50}
+        height={50}
+        className="w-12 h-12 p-2 hover:bg-gray-300 hover:rounded-full"
+      />
+      <button className="bg-black text-white px-2 py-1.5 rounded-lg">
+        Home
+      </button>
+      <button className="bg-gray-300 rounded-lg px-2 py-1.5">Create</button>
+
+      <div className="bg-gray-100 flex items-center w-full p-2 rounded-2xl">
+        <HiSearch className="text-[24px] " />
+        <input type="text" className="p-1 outline-none bg-transparent" placeholder="Search" />
+      </div>
+      <HiBell className="text-gray-500 text-3xl" />
+      <HiChat className="text-gray-500 text-3xl" />
+      <Image src="/images/profile.jpeg" alt="prof-img" width={50} height={50} className="w-12 h-8 bg-cover rounded-full"/>
+    </div>
+  );
+};
+
+export default Header;
