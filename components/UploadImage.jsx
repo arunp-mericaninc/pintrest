dc958a5b33947481029dae5be08f07b325d75daa
arunp-mericaninc <arunp@mericaninc.com>
2023-07-12 11:45:55 +0530

    Added window url

--- components/UploadImage.jsx
+++ components/UploadImage.jsx
@@ -1,39 +1,44 @@
-import React, { useState } from 'react'
-import { HiArrowUpCircle } from "react-icons/hi2";
+import React, { useState } from 'react';
+import { HiArrowUpCircle } from 'react-icons/hi2';
 
-const UploadImage=({setFile})=> {
-   const [selectedFile,setSelectedFile]=useState();
+const UploadImage = ({ setFile }) => {
+  const [selectedFile, setSelectedFile] = useState();
 
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+    setSelectedFile(e.target.files[0]);
+  };
 
   return (
-    <div className='h-[450px] bg-[#e9e9e9]
-    rounded-lg'>
-        
-        <label className='m-5 flex flex-col justify-center items-center
-        cursor-pointer h-[90%] 
-        border-[2px] border-gray-300 border-dashed rounded-lg text-gray-600 '>
-           
-          {!selectedFile?
-          <div className='flex items-center flex-col'>
-           <HiArrowUpCircle className='text-[22px]'/>   
-            <h2 className=' font-semibold'>Click to Upload</h2>
-            </div>
-            :null}
-            {selectedFile?
-            <img src={window.URL.createObjectURL(selectedFile)}
-            alt='selected-image'
+    <div className="h-[450px] bg-[#e9e9e9] rounded-lg">
+      <label
+        className="m-5 flex flex-col justify-center items-center cursor-pointer h-[90%] border-[2px] border-gray-300 border-dashed rounded-lg text-gray-600"
+        htmlFor="dropzone-file"
+      >
+        {!selectedFile ? (
+          <div className="flex items-center flex-col">
+            <HiArrowUpCircle className="text-[22px]" />
+            <h2 className="font-semibold">Click to Upload</h2>
+          </div>
+        ) : null}
+        {typeof window !== 'undefined' && selectedFile ? (
+          <img
+            src={window.URL.createObjectURL(selectedFile)}
+            alt="selected-image"
             width={500}
             height={800}
-            className='object-contain h-[90%]'
-            />:null}
-            <input id="dropzone-file" type="file"
-             className="hidden"  
-             onChange={(e)=>{setFile(e.target.files[0]);
-             setSelectedFile(e.target.files[0])}} />
-        
-        </label>
+            className="object-contain h-[90%]"
+          />
+        ) : null}
+        <input
+          id="dropzone-file"
+          type="file"
+          className="hidden"
+          onChange={handleFileChange}
+        />
+      </label>
     </div>
-  )
-}
+  );
+};
 
-export default UploadImage
+export default UploadImage;
