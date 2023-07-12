24c80da9d34fe1e7ec58d625a54619ef23283fea
arunp-mericaninc <arunp@mericaninc.com>
2023-07-12 12:57:34 +0530

    Added login page

--- components/Home/LoginScreen.jsx
+++ components/Home/LoginScreen.jsx
@@ -1,8 +1,15 @@
-import { signIn } from 'next-auth/react'
+import { signIn, useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 import {HiArrowSmRight} from 'react-icons/hi'
 
 const LoginScreen = () => {
+    const {data:session} = useSession()
+    const router = useRouter()
+
+    if(session!== null){
+        router.push("/")
+    }
   return (
     <div className='bg-gray-800 w-screen h-screen text-white flex flex-col items-center justify-center'>
         <p className='text-[3rem]'>To Continue Application</p>
